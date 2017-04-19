52f95680a3ca9431e72d0882ccf69b600ab27e56
Inessa Miller <i.inessa.miller@gmail.com>
2017-04-19 21:22:12 +0000

    code issue

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
 require_relative 'entry'
+require "csv"
 
 class AddressBook
     attr_reader :entries
@@ -6,7 +7,6 @@ class AddressBook
     def initialize
         @entries = []
     end
-end
 
  def add_entry(name, phone_number, email)
      
@@ -20,6 +20,17 @@ end
      end
  
      entries.insert(index, Entry.new(name, phone_number, email))
+   end
+ 
+   def import_from_csv(file_name)
+         csv_text = File.read(file_name)
+     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+   
+     csv.each do |row|
+       row_hash = row.to_hash
+       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+     end
+  end
  end
  
 
